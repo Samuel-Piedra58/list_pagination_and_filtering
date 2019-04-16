cc73f4308569ce2e9b82de9f8fb56476db2bff2e
Samuel Piedra <samuel58piedra@gmail.com>
2019-04-15 23:31:43 -0700

    Added Pagination Function and Filtering Pages Links
    Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # techdegree-project-02
+
+This project is part of the Treehouse Fullstack JavaScript Techdegree. This project belongs to Unit 02 - List Pagination and Filtering. The web page will paginate a list of students at 10 students per page and will allow the user to toggle between the sets (pages) of students.
--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 /*** 
    Add your global variables that store the DOM elements you will 
@@ -16,10 +15,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
-
-
-
+const listItems = document.querySelectorAll("li");
+const itemsPerPage = 10;
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
@@ -34,17 +31,56 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
-
-
-
-
+function showPage(list, page) {
+  const startIndex = parseInt(page) * itemsPerPage - itemsPerPage;
+  const endIndex = parseInt(page) * itemsPerPage - 1;
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i <= endIndex) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+}
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+function appendPageLinks(list) {
+  const pageDiv = document.querySelector(".page");
+  const div = document.createElement("div");
+  const ul = document.createElement("ul");
+  const pages = Math.ceil(list.length / itemsPerPage);
 
+  for (let i = 0; i < pages; i++) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    if (i === 0) {
+      a.className = "active";
+    }
+    a.setAttribute("href", "#");
+    a.innerHTML = `${i + 1}`;
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
 
+  div.className = "pagination";
+  div.appendChild(ul);
+  pageDiv.appendChild(div);
 
+  const pageLinks = document.querySelectorAll("li a");
 
+  for (let i = 0; i < pageLinks.length; i++) {
+    pageLinks[i].addEventListener("click", e => {
+      for (let i = 0; i < pageLinks.length; i++) {
+        pageLinks[i].className = "";
+      }
+      e.target.className = "active";
+      showPage(listItems, e.target.innerHTML);
+    });
+  }
+}
 
+showPage(listItems, 1);
+appendPageLinks(listItems);
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
