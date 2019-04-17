df67e3e81ac4090468f50c2e3e0a784666f36508
Samuel Piedra <samuel58piedra@gmail.com>
2019-04-16 21:25:29 -0700

    Refactor for v0.2

--- js/script.js
+++ js/script.js
@@ -3,34 +3,21 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// Globale variables
+// Variable to hold all student list items
 const listItems = document.querySelectorAll("li");
+// The number of list items to display per page
 const itemsPerPage = 10;
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+// showPage function will display a set number of list items
+// as a "page" by filtering out sets of list items
+/*
+  Function takes in two parameters:
+    list: this is the list of student items that needs to be paginated.
+    page: this is the set or paginated display of students.
+    For Example: showPage(listItems, 3) would display only those students
+    on the third page, students in positions (20-29)
+*/
 function showPage(list, page) {
   const startIndex = parseInt(page) * itemsPerPage - itemsPerPage;
   const endIndex = parseInt(page) * itemsPerPage - 1;
@@ -42,45 +29,60 @@ function showPage(list, page) {
     }
   }
 }
+
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 function appendPageLinks(list) {
+  // pageDiv is the parent div of the ul containing the list items
   const pageDiv = document.querySelector(".page");
   const div = document.createElement("div");
   const ul = document.createElement("ul");
   const pages = Math.ceil(list.length / itemsPerPage);
 
-  for (let i = 0; i < pages; i++) {
-    const li = document.createElement("li");
+  /*
+    Function createPageLinks(pageNum) returns an a tag with specific attribute
+    and innerHTML properties.
+      pageNum: This paramater corresponds to the index position link will have
+      amongst a collection of a tags
+  */
+  function createPageLinks(pageNum) {
     const a = document.createElement("a");
-    if (i === 0) {
+    if (pageNum === 0) {
       a.className = "active";
     }
-    a.setAttribute("href", "#");
-    a.innerHTML = `${i + 1}`;
+    a.setAttribute("href", `#`);
+    a.innerHTML = `${pageNum + 1}`;
+    return a;
+  }
+
+  // For each page created, create an li and a element and append them to the ul
+  for (let i = 0; i < pages; i++) {
+    const li = document.createElement("li");
+    const a = createPageLinks(i);
     li.appendChild(a);
     ul.appendChild(li);
   }
 
-  div.className = "pagination";
-  div.appendChild(ul);
-  pageDiv.appendChild(div);
-
-  const pageLinks = document.querySelectorAll("li a");
-
-  for (let i = 0; i < pageLinks.length; i++) {
-    pageLinks[i].addEventListener("click", e => {
-      for (let i = 0; i < pageLinks.length; i++) {
-        pageLinks[i].className = "";
+  ul.addEventListener("click", e => {
+    if (e.target.tagName === "A" || e.target.tagName === "LI") {
+      const links = ul.querySelectorAll("li a");
+      for (let i = 0; i < links.length; i++) {
+        links[i].className = "";
       }
       e.target.className = "active";
       showPage(listItems, e.target.innerHTML);
-    });
-  }
+    }
+  });
+
+  // Set newly create div
+  div.appendChild(ul);
+  div.className = "pagination";
+
+  // append newly created div to the first div in the body
+  pageDiv.appendChild(div);
 }
 
 showPage(listItems, 1);
 appendPageLinks(listItems);
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
